29a154572635a562c3d190ecd34dbf63598a0685
adil <wert-159@outlook.com>
2026-05-14 09:28:21 +0100

    add google auth with creating new users to the database

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 import morgan from "morgan";
 import {engine} from "express-handlebars";
 import indexRouter from './routes/index.js';
+import authRouter from './routes/auth.js';
+import passport from "passport";
+import session from "express-session";
+import connectDB from "./config/db.js";
+
+
+//filename && direname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 //load config 
 dotenv.config({path:"./config/config.env"})
+
+//passport config
+import setupPassport from './config/passport.js';
+setupPassport(passport);
+
 
 connectDB()
 
@@ -22,8 +38,23 @@ if (process.env.NODE_ENV==="development") {
 app.engine('.hbs', engine({defaultLayout:"main", extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+//session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+//passport middlewarre
+app.use(passport.initialize())
+app.use(passport.session())
+
+//static folder
+app.use(express.static(path.join(__dirname,"public")))
+
 //Routes
 app.use("/",indexRouter)
+app.use("/auth",authRouter)
 
 
 
